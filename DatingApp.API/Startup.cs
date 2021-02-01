5dd45c699ea8cf29cff0056331a0a495c90f981d
Madhusudhana Reddy Vangala <vmsrdy@gmail.com>
2021-02-01 21:50:11 +0530

    End of Section3

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -33,6 +33,8 @@ namespace DatingApp.API
                     options.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
                 });
             services.AddControllers();
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,10 +45,11 @@ namespace DatingApp.API
                 app.UseDeveloperExceptionPage();
             }
 
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
             app.UseRouting();
 
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
